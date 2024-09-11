53fd916aeb640537e448c679ea3735515941453c
Deathbaron <user@deathbaron.org>
2024-09-11 14:21:35 +0200

    moving crypto to library, fixing small things

--- Libs/LibCrypto/LibCrypto.lua
+++ Libs/LibCrypto/LibCrypto.lua
@@ -1,3 +1,12 @@
+--region LibStub
+
+local LIB_VERSION_MAJOR, LIB_VERSION_MINOR = "LibCrypto-1.0", 1
+
+local LibCrypto = LibStub:NewLibrary(LIB_VERSION_MAJOR, LIB_VERSION_MINOR)
+if not LibCrypto then
+    return
+end
+
 --#region X25519
 
 --[[
@@ -207,143 +216,164 @@ end
 
 --#region base64
 
--- Copyright (c) 2023 BernhardZat  -- see above
+--[[
+Copyright (c) 2018 Ilya Kolbin
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
-local base64 = {}
+]]--
 
-local extract = _G.bit32 and _G.bit32.extract -- Lua 5.2/Lua 5.3 in compatibility mode
-if not extract then
-	if _G.bit then -- LuaJIT
-		local shl, shr, band = _G.bit.lshift, _G.bit.rshift, _G.bit.band
-		extract = function( v, from, width )
-			return band( shr( v, from ), shl( 1, width ) - 1 )
-		end
-	elseif _G._VERSION == "Lua 5.1" then
-		extract = function( v, from, width )
-			local w = 0
-			local flag = 2^from
-			for i = 0, width-1 do
-				local flag2 = flag + flag
-				if v % flag2 >= flag then
-					w = w + 2^i
-				end
-				flag = flag2
-			end
-			return w
-		end
-	else -- Lua 5.3+
-		extract = load[[return function( v, from, width )
-			return ( v >> from ) & ((1 << width) - 1)
-		end]]()
-	end
+if true then
+
+    local base64 = {}
+
+    local extract = _G.bit32 and _G.bit32.extract -- Lua 5.2/Lua 5.3 in compatibility mode
+    if not extract then
+        if _G.bit then -- LuaJIT
+            local shl, shr, band = _G.bit.lshift, _G.bit.rshift, _G.bit.band
+            extract = function( v, from, width )
+                return band( shr( v, from ), shl( 1, width ) - 1 )
+            end
+        elseif _G._VERSION == "Lua 5.1" then
+            extract = function( v, from, width )
+                local w = 0
+                local flag = 2^from
+                for i = 0, width-1 do
+                    local flag2 = flag + flag
+                    if v % flag2 >= flag then
+                        w = w + 2^i
+                    end
+                    flag = flag2
+                end
+                return w
+            end
+        else -- Lua 5.3+
+            extract = load[[return function( v, from, width )
+                return ( v >> from ) & ((1 << width) - 1)
+            end]]()
+        end
+    end
+
+
+    function base64.makeencoder( s62, s63, spad )
+        local encoder = {}
+        for b64code, char in pairs{[0]='A','B','C','D','E','F','G','H','I','J',
+            'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',
+            'Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+            'o','p','q','r','s','t','u','v','w','x','y','z','0','1','2',
+            '3','4','5','6','7','8','9',s62 or '+',s63 or'/',spad or'='} do
+            encoder[b64code] = char:byte()
+        end
+        return encoder
+    end
+
+    function base64.makedecoder( s62, s63, spad )
+        local decoder = {}
+        for b64code, charcode in pairs( base64.makeencoder( s62, s63, spad )) do
+            decoder[charcode] = b64code
+        end
+        return decoder
+    end
+
+    local DEFAULT_ENCODER = base64.makeencoder()
+    local DEFAULT_DECODER = base64.makedecoder()
+
+    local char, concat = string.char, table.concat
+
+    function base64.encode( str, encoder, usecaching )
+        encoder = encoder or DEFAULT_ENCODER
+        local t, k, n = {}, 1, #str
+        local lastn = n % 3
+        local cache = {}
+        for i = 1, n-lastn, 3 do
+            local a, b, c = str:byte( i, i+2 )
+            local v = a*0x10000 + b*0x100 + c
+            local s
+            if usecaching then
+                s = cache[v]
+                if not s then
+                    s = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[extract(v,6,6)], encoder[extract(v,0,6)])
+                    cache[v] = s
+                end
+            else
+                s = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[extract(v,6,6)], encoder[extract(v,0,6)])
+            end
+            t[k] = s
+            k = k + 1
+        end
+        if lastn == 2 then
+            local a, b = str:byte( n-1, n )
+            local v = a*0x10000 + b*0x100
+            t[k] = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[extract(v,6,6)], encoder[64])
+        elseif lastn == 1 then
+            local v = str:byte( n )*0x10000
+            t[k] = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[64], encoder[64])
+        end
+        return concat( t )
+    end
+
+    function base64.decode( b64, decoder, usecaching )
+        decoder = decoder or DEFAULT_DECODER
+        local pattern = '[^%w%+%/%=]'
+        if decoder then
+            local s62, s63
+            for charcode, b64code in pairs( decoder ) do
+                if b64code == 62 then s62 = charcode
+                elseif b64code == 63 then s63 = charcode
+                end
+            end
+            pattern = ('[^%%w%%%s%%%s%%=]'):format( char(s62), char(s63) )
+        end
+        b64 = b64:gsub( pattern, '' )
+        local cache = usecaching and {}
+        local t, k = {}, 1
+        local n = #b64
+        local padding = b64:sub(-2) == '==' and 2 or b64:sub(-1) == '=' and 1 or 0
+        for i = 1, padding > 0 and n-4 or n, 4 do
+            local a, b, c, d = b64:byte( i, i+3 )
+            local s
+            if usecaching then
+                local v0 = a*0x1000000 + b*0x10000 + c*0x100 + d
+                s = cache[v0]
+                if not s then
+                    local v = decoder[a]*0x40000 + decoder[b]*0x1000 + decoder[c]*0x40 + decoder[d]
+                    s = char( extract(v,16,8), extract(v,8,8), extract(v,0,8))
+                    cache[v0] = s
+                end
+            else
+                local v = decoder[a]*0x40000 + decoder[b]*0x1000 + decoder[c]*0x40 + decoder[d]
+                s = char( extract(v,16,8), extract(v,8,8), extract(v,0,8))
+            end
+            t[k] = s
+            k = k + 1
+        end
+        if padding == 1 then
+            local a, b, c = b64:byte( n-3, n-1 )
+            local v = decoder[a]*0x40000 + decoder[b]*0x1000 + decoder[c]*0x40
+            t[k] = char( extract(v,16,8), extract(v,8,8))
+        elseif padding == 2 then
+            local a, b = b64:byte( n-3, n-2 )
+            local v = decoder[a]*0x40000 + decoder[b]*0x1000
+            t[k] = char( extract(v,16,8))
+        end
+        return concat( t )
+    end
+
+    LibCrypto.Base64 = base64
 end
-
-
-function base64.makeencoder( s62, s63, spad )
-	local encoder = {}
-	for b64code, char in pairs{[0]='A','B','C','D','E','F','G','H','I','J',
-		'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',
-		'Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-		'o','p','q','r','s','t','u','v','w','x','y','z','0','1','2',
-		'3','4','5','6','7','8','9',s62 or '+',s63 or'/',spad or'='} do
-		encoder[b64code] = char:byte()
-	end
-	return encoder
-end
-
-function base64.makedecoder( s62, s63, spad )
-	local decoder = {}
-	for b64code, charcode in pairs( base64.makeencoder( s62, s63, spad )) do
-		decoder[charcode] = b64code
-	end
-	return decoder
-end
-
-local DEFAULT_ENCODER = base64.makeencoder()
-local DEFAULT_DECODER = base64.makedecoder()
-
-local char, concat = string.char, table.concat
-
-function base64.encode( str, encoder, usecaching )
-	encoder = encoder or DEFAULT_ENCODER
-	local t, k, n = {}, 1, #str
-	local lastn = n % 3
-	local cache = {}
-	for i = 1, n-lastn, 3 do
-		local a, b, c = str:byte( i, i+2 )
-		local v = a*0x10000 + b*0x100 + c
-		local s
-		if usecaching then
-			s = cache[v]
-			if not s then
-				s = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[extract(v,6,6)], encoder[extract(v,0,6)])
-				cache[v] = s
-			end
-		else
-			s = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[extract(v,6,6)], encoder[extract(v,0,6)])
-		end
-		t[k] = s
-		k = k + 1
-	end
-	if lastn == 2 then
-		local a, b = str:byte( n-1, n )
-		local v = a*0x10000 + b*0x100
-		t[k] = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[extract(v,6,6)], encoder[64])
-	elseif lastn == 1 then
-		local v = str:byte( n )*0x10000
-		t[k] = char(encoder[extract(v,18,6)], encoder[extract(v,12,6)], encoder[64], encoder[64])
-	end
-	return concat( t )
-end
-
-function base64.decode( b64, decoder, usecaching )
-	decoder = decoder or DEFAULT_DECODER
-	local pattern = '[^%w%+%/%=]'
-	if decoder then
-		local s62, s63
-		for charcode, b64code in pairs( decoder ) do
-			if b64code == 62 then s62 = charcode
-			elseif b64code == 63 then s63 = charcode
-			end
-		end
-		pattern = ('[^%%w%%%s%%%s%%=]'):format( char(s62), char(s63) )
-	end
-	b64 = b64:gsub( pattern, '' )
-	local cache = usecaching and {}
-	local t, k = {}, 1
-	local n = #b64
-	local padding = b64:sub(-2) == '==' and 2 or b64:sub(-1) == '=' and 1 or 0
-	for i = 1, padding > 0 and n-4 or n, 4 do
-		local a, b, c, d = b64:byte( i, i+3 )
-		local s
-		if usecaching then
-			local v0 = a*0x1000000 + b*0x10000 + c*0x100 + d
-			s = cache[v0]
-			if not s then
-				local v = decoder[a]*0x40000 + decoder[b]*0x1000 + decoder[c]*0x40 + decoder[d]
-				s = char( extract(v,16,8), extract(v,8,8), extract(v,0,8))
-				cache[v0] = s
-			end
-		else
-			local v = decoder[a]*0x40000 + decoder[b]*0x1000 + decoder[c]*0x40 + decoder[d]
-			s = char( extract(v,16,8), extract(v,8,8), extract(v,0,8))
-		end
-		t[k] = s
-		k = k + 1
-	end
-	if padding == 1 then
-		local a, b, c = b64:byte( n-3, n-1 )
-		local v = decoder[a]*0x40000 + decoder[b]*0x1000 + decoder[c]*0x40
-		t[k] = char( extract(v,16,8), extract(v,8,8))
-	elseif padding == 2 then
-		local a, b = b64:byte( n-3, n-2 )
-		local v = decoder[a]*0x40000 + decoder[b]*0x1000
-		t[k] = char( extract(v,16,8))
-	end
-	return concat( t )
-end
-
-_G.Base64 = base64
 
 --#endregion
 
@@ -608,7 +638,7 @@ if true then
         end
     end
     
-    local function inv_mixColumns(state) -- TODO: fix
+    local function inv_mixColumns(state)
         for i = 0, 3 do
             local cur = i*4+1
             local a, b, c, d = state[cur], state[cur + 1], state[cur + 2], state[cur + 3]
@@ -731,7 +761,7 @@ if true then
         return table.concat(text)
     end
 
-    _G.AES = {
+    LibCrypto.AES = {
         encrypt = encrypt;
         decrypt = decrypt;
         ECB_256 = ECB_256;
@@ -740,8 +770,9 @@ end
 
 --#endregion
 
+--#region misc
 
-local function table_to_zero_indexed(tbl)
+function LibCrypto.table_to_zero_indexed(tbl)
     for i = 1, #tbl do
         tbl[i-1] = tbl[i]
     end
@@ -749,7 +780,7 @@ local function table_to_zero_indexed(tbl)
     return tbl
 end
 
-local function print_hex_table(tbl)
+function LibCrypto.print_hex_table(tbl)
     local str_tbl = {}
     for i = 0, #tbl do
         str_tbl[#str_tbl + 1] = string.format("%02x", tbl[i])
@@ -760,76 +791,4 @@ local function print_hex_table(tbl)
     print(table.concat(str_tbl, ""))
 end
 
-local sk, pk, esk, epk
-if false then
-    sk, pk = X25519.generate_keypair()
-    esk, epk = X25519.generate_keypair()
-elseif true then
-    sk = table_to_zero_indexed({0x38,0xb5,0x62,0x0d,0x2d,0x93,0x1a,0xe3,0x63,0x64,0xc6,0x33,0xf0,0x11,0x31,0x1f,0xc2,0x13,0x46,0x88,0x54,0xa7,0x9e,0x25,0x31,0xdf,0xc4,0x28,0x62,0x41,0x07,0x6d})
-    pk = table_to_zero_indexed({0xa3,0x05,0xe5,0x8f,0xa7,0x4d,0xc5,0x7e,0xa8,0x32,0xc8,0x1a,0x69,0x79,0xbe,0xf5,0xa8,0x97,0x64,0x06,0xa4,0x9e,0xb9,0x43,0xfb,0x97,0x95,0x33,0x5d,0x1f,0xf8,0x44})
-    esk = table_to_zero_indexed({0x98,0xb2,0x88,0x88,0xf8,0x3d,0xcf,0xc9,0x7b,0x9f,0xea,0x36,0x2a,0x3d,0x71,0x0c,0x08,0x73,0xc5,0xe2,0xb7,0x9e,0xb9,0xa7,0xd3,0xaf,0xb4,0xcc,0x40,0x2d,0x6a,0x54})
-    epk = table_to_zero_indexed({0x35,0x66,0x97,0xed,0x50,0x90,0x59,0xc6,0x18,0xad,0xec,0x42,0xc2,0x3d,0x8b,0xd4,0x0e,0x70,0xb4,0x43,0x14,0x63,0x77,0xe2,0xb0,0x62,0xfa,0xe7,0x0f,0xc5,0x4a,0x34})
-else
-    sk = table_to_zero_indexed({0xf1,0xd2,0xae,0xa0,0xf9,0x52,0x50,0x58,0x1e,0x15,0x42,0x82,0x42,0x83,0x02,0x1a,0x3c,0x47,0x54,0x30,0x11,0x92,0x69,0x36,0x89,0x3a,0xdd,0x25,0xf9,0x3c,0xa9,0x58})
-    pk = table_to_zero_indexed({0x19,0x68,0x35,0xd8,0x9e,0xc2,0x29,0xf0,0xd8,0x1b,0x66,0x91,0xa5,0x2c,0x37,0x5a,0xeb,0xd3,0xce,0xd6,0x73,0x97,0x64,0xbe,0xfe,0x6a,0x80,0xed,0xa1,0x85,0xaa,0x20})
-    esk = table_to_zero_indexed({0x30,0x0b,0xa6,0x9a,0xa8,0x3f,0xd2,0x3d,0xeb,0x8e,0xa7,0x61,0xeb,0x7d,0x0e,0xbf,0x64,0x03,0x41,0x84,0xbb,0x0f,0xe9,0x56,0x6c,0x0d,0xed,0xfe,0xd0,0xf6,0x05,0x34})
-    epk = table_to_zero_indexed({0x03,0xdd,0x5f,0x8f,0x34,0x94,0x1a,0x90,0x30,0x2b,0x94,0x3c,0x5d,0xeb,0x1e,0x75,0xfe,0xbe,0xef,0xfd,0xe1,0x70,0x93,0x79,0x32,0xa7,0x56,0xeb,0x4c,0x2c,0xc5,0x28})
-    -- 3410e8c90f834a02ca 2e436e676e16bbf3 b92c3d5af5b9551e ffbbf06c2e2277
-end
-
-print("----")
-print_hex_table(sk)
-print_hex_table(pk)
-print_hex_table(esk)
-print_hex_table(epk)
-print("----")
-print_hex_table(X25519.get_shared_key(sk, epk))
-print("----")
-local shared_secret = X25519.get_shared_key(esk, pk)
-print_hex_table(shared_secret)
-print("----")
-
-
-local key = table_to_zero_indexed({0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1a,0x1b,0x1c,0x1d,0x1e,0x1f})
---key = 0x0
-local plaintext = Base64.decode('ABEiM0RVZneImaq7zN3u/w==') -- 00112233445566778899aabbccddeeff
-local cyphertext = AES.ECB_256(AES.encrypt, key, plaintext)
-local b64_cyphertext = Base64.encode(cyphertext)
-local cyphertext2 = Base64.decode(b64_cyphertext)
-local plaintext2 = AES.ECB_256(AES.decrypt, key, cyphertext2)
-
-
-print("plaintext:", #plaintext, plaintext)
-print("cyphertext:", #cyphertext, b64_cyphertext)
-print("plaintext:", #plaintext2, plaintext2)
-
---[[
-
-
-keep sk, send pk + esk to receiver, discard epk
-use the 32 bytes shared key as aes key to encrypt/decrypt
---------------
-
-from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
-
-def print_as_lua_table(lst):
-    print("{"+",".join(["0x%02x" % b for b in lst])+"}")
-    
-def print_hex(lst):
-    print(",".join(["%02x" % b for b in lst]))
-
-def generate_keypair():
-    sk = X25519PrivateKey.generate()
-    pk = sk.public_key()
-    return sk, pk
-
-sk, pk = generate_keypair()
-esk, epk = generate_keypair()
-print_as_lua_table(sk.private_bytes_raw())
-print_as_lua_table(pk.public_bytes_raw())
-print_as_lua_table(esk.private_bytes_raw())
-print_as_lua_table(epk.public_bytes_raw())
-
-shared_key = sk.exchange(epk)
-print_hex(shared_key)
-]]--
+--#endregion
